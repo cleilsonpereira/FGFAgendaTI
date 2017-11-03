90805d230461a0e76a7584be8f82f3567b21a959
cleilsonpereira <cleilsonpereira@aluno.fgf.edu.br>
2017-11-03 23:13:09 +0000

    update login-page.component.ts

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {AF} from "../../providers/af";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public afService: AF, private router: Router) { }
+  login() {
+	this.afService.loginWithGoogle().then((data) => {
+	// Send them to the homepage if they are logged in 
+	this.router.navigate(['']);
+	})
   }
+
 
 }
