3eb6080b9ba8d18e863dde37ceccbef673c75289
cleilsonpereira <cleilsonpereira@aluno.fgf.edu.br>
2017-11-05 06:12:29 +0000

    update environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBPNFiXoYNVZg3jmI_yPIWiTFGP2acPSqY",
+    authDomain: "agenda-ti.firebaseapp.com",
+    databaseURL: "https://agenda-ti.firebaseio.com",
+    projectId: "agenda-ti",
+    storageBucket: "agenda-ti.appspot.com",
+    messagingSenderId: "1045975167017"
+  }
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,16 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPNFiXoYNVZg3jmI_yPIWiTFGP2acPSqY",
+    authDomain: "agenda-ti.firebaseapp.com",
+    databaseURL: "https://agenda-ti.firebaseio.com",
+    projectId: "agenda-ti",
+    storageBucket: "agenda-ti.appspot.com",
+    messagingSenderId: "1045975167017"
+
+  }
 };
+
+
