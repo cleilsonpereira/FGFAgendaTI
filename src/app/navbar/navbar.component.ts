b377fd040b4ee7c724188034ae01b50eacac00f2
cleilsonpereira <cleilsonpereira@aluno.fgf.edu.br>
2017-11-05 21:13:28 +0000

    folder navbar content include

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../shared/authentication.service';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authServ: AuthenticationService, private router: Router) { }
+  
+  user: Observable<firebase.User>;
 
   ngOnInit() {
+    this.user = this.authServ.authUser();
+  }
+
+  signout() {
+    this.authServ.logout().then(onResolve => this.router.navigate(['/']));
   }
 
 }
+
