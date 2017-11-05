c39e69d66907f78b6491b2663a96e5ecb39401cc
cleilsonpereira <cleilsonpereira@aluno.fgf.edu.br>
2017-11-05 20:46:31 +0000

    files in folder sign-in content insert

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../shared/authentication.service';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
 
-  constructor() { }
+  constructor(private authServ: AuthenticationService, private router: Router) {
+  }
 
-  ngOnInit() {
+  msg: string;
+  email: string;
+  password: string;
+
+  signin() {
+    this.authServ.login({ email: this.email, password: this.password })
+      .then(resolve => this.router.navigate(['cardView']))
+      .catch(error => this.msg = error.message);
   }
 
 }
+
