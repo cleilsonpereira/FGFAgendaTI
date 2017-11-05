18634a3602ae53d18fc9896920b01fe5311a12fd
cleilsonpereira <cleilsonpereira@aluno.fgf.edu.br>
2017-11-05 21:24:51 +0000

    list-container insert

--- src/app/list-container/list-container.component.ts
+++ src/app/list-container/list-container.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../shared/product.service';
+import { Observable } from 'rxjs/Observable';
+import { Product } from '../shared/product.model';
 
 @Component({
   selector: 'app-list-container',
   templateUrl: './list-container.component.html',
-  styleUrls: ['./list-container.component.css']
+  styleUrls: ['./list-container.component.scss']
 })
 export class ListContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private prodServ: ProductService) { }
 
   ngOnInit() {
+    this.products = this.prodServ.getProducts();
   }
 
+  products: Observable<Product[]>;
+
 }
+
