333cdaf5b740dbb3ed854370561ff0ce1ec2890a
cleilsonpereira <cleilsonpereira@aluno.fgf.edu.br>
2017-11-05 20:53:50 +0000

    files in the folder produts-table insert content

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ProductService } from '../shared/product.service';
+import { Observable } from 'rxjs/Rx';
+import { Product } from '../shared/product.model';
 
 @Component({
   selector: 'app-products-table',
   templateUrl: './products-table.component.html',
-  styleUrls: ['./products-table.component.css']
+  styleUrls: ['./products-table.component.scss']
 })
 export class ProductsTableComponent implements OnInit {
+  
+  products: Observable<Product[]>;
 
-  constructor() { }
+  constructor(private prodServ: ProductService) { }
 
   ngOnInit() {
+    this.products = this.prodServ.getProducts();
   }
 
+  @Output() selectProduct = new EventEmitter<Product>();
+  @Output() add = new EventEmitter<boolean>();
+
+  selectedProduct(product: Product) {
+    this.selectProduct.emit(product);
+    this.add.emit(false);
+  }
+
+  initializeNew() {
+    this.selectProduct.emit(this.prodServ.initializeNew());
+    this.add.emit(true);
+  }
+  
 }
+
